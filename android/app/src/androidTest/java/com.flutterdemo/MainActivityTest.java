fde1b0418e0e283c9fbb49866e033a5c7162fe8d
kc2040 <niranjan.kc@randstadusa.com>
2021-03-19 22:22:20 -0500

    instrumentation updated

--- android/app/src/androidTest/java/com.flutterdemo/MainActivityTest.java
+++ android/app/src/androidTest/java/com.flutterdemo/MainActivityTest.java
@@ -1,9 +1,9 @@
-package com.flutter_demoapp;
+package com.flutterdemo;
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_rest.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-import com.flutter_demoapp.MainActivity;
+import com.flutterdemo.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public  class MainActivityTest{
